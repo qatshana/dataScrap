27879f540e261ed4691658c484156c4ca990cf28
Alex Qatshan <qatshan@gmail.com>
2018-04-19 14:23:04 -0400

    factored code and move code into fuctions

--- readCompanyInfo.py
+++ readCompanyInfo.py
@@ -1,16 +1,13 @@
-import json
+import readMubasherHtmlFunc as rm
 
-#tickers=['1090','1050','1040','1020','1010','1060','1080','1120','2190','2030','2150','6010','6020','6040','4010','2160','3010','3020','2170','3030','3040','3050','3060','3080','3090','2180','2010','2020','2200']
+tickers=['1090','1050','1040','1020','1010','1060','1080','1120','2190','2030','2150','6010','6020','6040','4010','2160','3010','3020','2170','3030','3040','3050','3060','3080','3090','2180','2010','2020','2200']
 #tickers=['DIB']
 #tickers=['1090']
-tickers=['COMI']
+#tickers=['COMI']
+dataList=[]
 for ticker in tickers:
 	fname=ticker+'-FullData.json'
-
-	with open(fname, "r") as fr:
-		data=fr.read()
-
-	dictData=json.loads(data)
+	dictData=rm.readFileJson(fname)
 
 	print (dictData['Description'])
 
@@ -18,5 +15,8 @@ for ticker in tickers:
 
 	#print (dictData['P/B Ratio'])
 	print (dictData['Company Name'],dictData['Market Cap'],dictData['Close Price'],dictData['Price Change %'])
+	data=[dictData['Company Name'],dictData['Market Cap'],dictData['Close Price'],dictData['Price Change %']]
+	dataList.append(data)
 	#print (len(tickers))
 	#print (dictData['Market Cap'])
+print (dataList)
--- readMubasherHtmlFunc.py
+++ readMubasherHtmlFunc.py
@@ -1,39 +1,39 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 import json
-
 from selenium import webdriver
 from time import sleep
 import io
-
 from datetime import datetime
 
 def readFile(fname):
 	with io.open(fname, "r", encoding="utf-8") as fr:
 		data=fr.read()	
-	soup = BeautifulSoup(data)
+	soup = BeautifulSoup(data,"html.parser")
 	return soup	
 
 def readFile2(fname):
 	with open(fname, "r") as fr:
 		data=fr.read()	
-	soup = BeautifulSoup(data)
+	soup = BeautifulSoup(data,"html.parser")
 	return soup	
+def readFileJson(fname):	
+	with open(fname, "r") as fr:
+		data=fr.read()
+	dictData=json.loads(data)
+	return dictData
 
 def readMubasherDataProfile(ticker):
-
 	fname=ticker+'-Profile.html'
 	soup=readFile(fname)
 	val1='mi-section__title'
 	companyName=soup.find('h1', {'class': val1})
 	d0=dict([('Company Name',companyName.text)])
-
 	'''
 	get company's name and description
 	'''
 	val1='company-profile__general-information__text2'	
-	companyProfile=soup.findAll('span', {'class': val1})
-	
+	companyProfile=soup.findAll('span', {'class': val1})	
 	companyName=companyProfile[0].text
 	companyDescription=companyProfile[1].text
 	print (companyDescription)
@@ -43,8 +43,7 @@ def readMubasherDataProfile(ticker):
 	Get market stats
 	'''
 	val1='market-summary__block-row'
-	marketBlock=soup.findAll('div', {'class': val1})
-	
+	marketBlock=soup.findAll('div', {'class': val1})	
 	val1='market-summary__block-text'
 	item1='market-summary__block-number'
 	items=[]
@@ -55,8 +54,7 @@ def readMubasherDataProfile(ticker):
 		value=market.find('span', {'class': item1})
 		items.append(item.text)
 		values.append(value.text)
-		d2.update(dict([(item.text,value.text)]))
-	
+		d2.update(dict([(item.text,value.text)]))	
 	d3=dict([('Market Stats',d2)])
 
 	'''
@@ -71,7 +69,6 @@ def readMubasherDataProfile(ticker):
 			name=shareholder.find('span')			
 			number=shareholder.find('span', {'class': 'number'})
 			if (number!=None):
-				print(number.text)
 				names.append(name.text)
 				numbers.append(number.text)
 	d4=dict([('Shareholder Name',names)])
@@ -86,7 +83,6 @@ def readMubasherDataProfile(ticker):
 	'''
 	managementTag='company-profile__management md-whiteframe-z1__nested'
 	management=soup.findAll('div', {'class': managementTag})
-
 	managementTag2='company-profile__management__item'
 	managementList=management[0].find_all('div', {'class': managementTag2})
 	mgrNames=[]
@@ -95,7 +91,6 @@ def readMubasherDataProfile(ticker):
 		mgrName=manager.find('span', {'class': 'company-profile__management__text1'})
 		mgrTitle=manager.find('span', {'class': 'company-profile__management__text2'})
 		if (mgrName!=None):
-			print(mgrName.text,mgrTitle.text)
 			mgrNames.append(mgrName.text)
 			mgrTitles.append(mgrTitle.text)
 
@@ -104,28 +99,21 @@ def readMubasherDataProfile(ticker):
 	d10={}
 	d10.update(d8)
 	d10.update(d9)
-	d11=dict([('Management Team',d10)])
-	
+	d11=dict([('Management Team',d10)])	
 	dfinal=d0
 	dfinal.update(d1)
 	dfinal.update(d3)
 	dfinal.update(d7)
-	dfinal.update(d11)
-	#print (dfinal)		
-	dfinalJson=json.dumps(dfinal)		
-	fname=ticker+"-Desc.json"
-	with open(fname, "w") as fw:
-		fw.write(dfinalJson)				
-	return (dfinalJson)	
+	dfinal.update(d11)				
+	return (dfinal)	
 
 
-def readMubasherDataStock(ticker,dfinalJson):	
+def readMubasherDataStock(ticker,dfinal):	
 	'''
 	read stocks.html to get outstanding shares
 	'''	
-	#fname='stock.html'  # name to downloaded html file		
 	fname=ticker+'-Stock.html'
-	soup=readFile2(fname)		
+	soup=readFile(fname)		
 	val1='stock-overview__text-and-value-item'
 	stockList=soup.find_all('div', {'class': val1})	
 	d20={}
@@ -133,7 +121,6 @@ def readMubasherDataStock(ticker,dfinalJson):
 		itemName=stock.find('span', {'class': 'stock-overview__text'})
 		itemValue=stock.find('span', {'class': 'number number--aligned'})
 		if (itemValue!=None):
-			print (itemName.text,itemValue.text)
 			d20.update(dict([(itemName.text,itemValue.text)]))
 	
 	val1='market-summary__last-price'
@@ -145,126 +132,25 @@ def readMubasherDataStock(ticker,dfinalJson):
 	d26=d22
 	d26.update(d24)
 	d26.update(d20)
-	dfinalJson=json.dumps(dfinal)
-	fname=ticker+'-FullData.json'
+	dfinal.update(d26)	
+	return dfinal	
+
+def saveJsonFile(file,d):
+	dJson=json.dumps(d)
 	with open(fname, "w") as fw:
-		fw.write(dfinalJson)
-	return (dfinalJson)	
+		fw.write(dJson)
+	
 
 if __name__ == "__main__":
 	#tickers=['1090','1050','1040','1020','1010','1060','1080','1120','2190','2030','2150','6010','6020','6040','4010','2160','3010','3020','2170','3030','3040','3050','3060','3080','3090','2180','2010','2020','2200']	
-	#tickers=['1090']
+	tickers=['1090']
 	#tickers=['DIB']
-	tickers=['COMI']
+	#tickers=['COMI']
 
 	for ticker in tickers:
-		fname=ticker+'-Profile.html'
-		soup=readFile(fname)
-		val1='mi-section__title'
-		companyName=soup.find('h1', {'class': val1})
-		d0=dict([('Company Name',companyName.text)])		
-		val1='company-profile__general-information__text2'	
-		companyProfile=soup.findAll('span', {'class': val1})
-		companyName=companyProfile[0].text
-		companyDescription=companyProfile[1].text
-		print (companyDescription)
-		d1=dict([('Description',companyDescription)])		
-		val1='market-summary__block-row'
-		marketBlock=soup.findAll('div', {'class': val1})
-		
-		val1='market-summary__block-text'
-		item1='market-summary__block-number'
-		items=[]
-		values=[]
-		d2={}
-		for market in marketBlock:
-			item=market.find('span', {'class': val1})
-			value=market.find('span', {'class': item1})
-			items.append(item.text)
-			values.append(value.text)
-			d2.update(dict([(item.text,value.text)]))
-		
-		d3=dict([('Market Stats',d2)])		
-		shareHoldersTag='md-whiteframe-z1__nested'
-		shareholders=soup.findAll('ul', {'class': shareHoldersTag})	
-		shareholdersList=shareholders[0].find_all('li')
-		names=[]
-		numbers=[]
-		for shareholder in shareholdersList:
-				name=shareholder.find('span')			
-				number=shareholder.find('span', {'class': 'number'})
-				if (number!=None):
-					print(number.text)
-					names.append(name.text)
-					numbers.append(number.text)
-		d4=dict([('Shareholder Name',names)])
-		d5=dict([('Shareholder Stake',numbers)])
-		d6={}
-		d6.update(d4)
-		d6.update(d5)
-		d7=dict([('Shareholders',d6)])		
-		managementTag='company-profile__management md-whiteframe-z1__nested'
-		management=soup.findAll('div', {'class': managementTag})
-
-		managementTag2='company-profile__management__item'
-		managementList=management[0].find_all('div', {'class': managementTag2})
-		mgrNames=[]
-		mgrTitles=[]
-		for manager in managementList:
-			mgrName=manager.find('span', {'class': 'company-profile__management__text1'})
-			mgrTitle=manager.find('span', {'class': 'company-profile__management__text2'})
-			if (mgrName!=None):
-				print(mgrName.text,mgrTitle.text)
-				mgrNames.append(mgrName.text)
-				mgrTitles.append(mgrTitle.text)
-
-		d8=dict([('Name',mgrNames)])
-		d9=dict([('Title',mgrTitles)])
-		d10={}
-		d10.update(d8)
-		d10.update(d9)
-		d11=dict([('Management Team',d10)])
-		
-		dfinal=d0
-		dfinal.update(d1)
-		dfinal.update(d3)
-		dfinal.update(d7)
-		dfinal.update(d11)
-		print (dfinal)
-		dfinalJson=json.dumps(dfinal)
-
+		d1=readMubasherDataProfile(ticker)
+		d2=readMubasherDataStock(ticker,d1)
 		fname=ticker+"-Desc.json"
-		with open(fname, "w") as fw:
-			fw.write(dfinalJson)				
-			
-		fname=ticker+'-Stock.html'
-		soup=readFile2(fname)
-		
-
-		val1='stock-overview__text-and-value-item'
-		stockList=soup.find_all('div', {'class': val1})	
-		d20={}
-		for stock in stockList:
-			itemName=stock.find('span', {'class': 'stock-overview__text'})
-			itemValue=stock.find('span', {'class': 'number number--aligned'})
-			if (itemValue!=None):
-				print (itemName.text,itemValue.text)
-				d20.update(dict([(itemName.text,itemValue.text)]))
-		
-		val1='market-summary__last-price'
-		closePrice=soup.find('div', {'class': val1}).text	
-		
-		d22=dict([('Close Price',closePrice)])
-		val1='market-summary__change-percentage'
-		priceChange=soup.find('div', {'class': val1})
-		d24=dict([('Price Change %',priceChange.text)])
-		d26=d22
-		d26.update(d24)
-		d26.update(d20)
-
-		dfinal.update(d26)
-
-		dfinalJson=json.dumps(dfinal)
+		saveJsonFile(fname,d1)
 		fname=ticker+'-FullData.json'
-		with open(fname, "w") as fw:
-			fw.write(dfinalJson)		
+		saveJsonFile(fname,d2)
